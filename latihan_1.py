04d51e1dc6eb60e093a12ba479f9f84bf2037f98
pandupoetra <142768532+pandupoetra@users.noreply.github.com>
2023-10-24 04:59:27 +0700

    Add files via upload

---
+++ latihan_1.py
@@ -0,0 +1,17 @@
+#Matriks dengan ukuran 2x2 
+matriksA = [[1,0],[0,1]]  
+matrix = [[5, 0], [2, 6],] 
+print(matriksA)
+print(matrix)
+#Matriks dengan ukuran 3x3 
+matriksB = [[1,0,1], [0,1,0],[1,0,1]]  
+matrix1 = [[5, 0, 8],[2, 6, 7],[1, 3, 4], ]  
+print(matriksB) 
+print(matrix1)
+#Matriks dengan ukuran 4x4 
+matriksC = [[1,0,1,0],[0,1,0,1],[1,0,1,0],[0,1,0,1]] 
+matrix2 = [[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 1, 0],[0, 0, 0, 1],]
+print(matriksC)   
+print(matrix2)
+
+# Output program:  #Matriks A  [[1,0], [0,1]]   matrix = [[5, 0],[2, 6]]  #Matriks B  [[1,0,1], [0,1,0], [1,0,1]]   matrix1 = [ [5, 0, 8],[2, 6, 7],[1, 3, 4]]  #Matriks C  [[1,0,1,0], [0,1,0,1], [1,0,1,0], [0,1,0,1]]  matrix2 = [[1, 0, 0, 0],[0, 1, 0, 0],[0, 0, 1, 0], [0, 0, 0, 1]]  
---
+++ latihan_2.py
@@ -0,0 +1,7 @@
+m = 4  
+n = 5  
+x = [0]*m
+print(x)
+for i in range(m):      
+    x[i] = [1]*n  
+print (x)
